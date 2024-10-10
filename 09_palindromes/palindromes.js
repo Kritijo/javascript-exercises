216e26ba560203c9d849a4ca0d025cc45f9a94fb
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-10-10 17:37:13 +0530

    Add logic to palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let punctuations = ["!",".",","," "];
+    string = string
+    .toLowerCase()
+    .split('')
+    .filter(char=>!punctuations.includes(char))
+    .join('')
 
+    let revString = string.split('').reverse().join('');
+    return string === revString;
 };
 
 // Do not edit below this line
