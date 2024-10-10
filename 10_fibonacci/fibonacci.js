0476aa440e6e2b873cd99679af69395a41363156
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-10-10 18:07:29 +0530

    Implement Fibonacci sequence logic using destructuring assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let prev = 0;
+    let curr = 1;
+    n = Number(n);
+    if(n == 0) return 0;
+    if(n < 0) return 'OOPS';
+    for (let i = 1; i<n; i++){
+        [[prev],[curr]] = [[curr],[curr+prev]];
+    }
+    return curr; 
 };
 
 // Do not edit below this line
