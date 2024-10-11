842e8be12d10b0f755db2f3ae703ae1efe81832e
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-10-11 10:17:15 +0530

    Implement logic to find the oldest person using reduce function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const getAge = (birth,death)=>{
+    let currYear = new Date().getFullYear();
+    currYear = death || currYear;
+    return currYear-birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest,currPerson)=>{
+        let oldestAge = getAge(oldest.yearOfBirth,oldest.yearOfDeath);
+        let personAge = getAge(currPerson.yearOfBirth,currPerson.yearOfDeath);
+        return oldestAge < personAge ? currPerson : oldest 
+    })
 };
 
 // Do not edit below this line
